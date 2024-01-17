3bc996bd4dc3157a92ef9b0789c5867d86584772
“SmilingIT” <jeremy@smilingit.fr>
2024-01-17 22:15:29 +0100

    add new param github-registration-token

--- src/config.js
+++ src/config.js
@@ -6,6 +6,7 @@ class Config {
     this.input = {
       mode: core.getInput('mode'),
       githubToken: core.getInput('github-token'),
+      githubRegistrationToken: core.getInput('github-registration-token'),
       ec2ImageId: core.getInput('ec2-image-id'),
       ec2InstanceType: core.getInput('ec2-instance-type'),
       subnetId: core.getInput('subnet-id'),
@@ -41,8 +42,8 @@ class Config {
       throw new Error(`The 'mode' input is not specified`);
     }
 
-    if (!this.input.githubToken) {
-      throw new Error(`The 'github-token' input is not specified`);
+    if (!this.input.githubToken && !this.input.githubRegistrationToken) {
+      throw new Error(`The 'github-token' xor 'github-registration-token' inputs must be specified`);
     }
 
     if (this.input.mode === 'start') {
--- src/index.js
+++ src/index.js
@@ -10,16 +10,28 @@ function setOutput(label, ec2InstanceId) {
 
 async function start() {
   const label = config.generateUniqueLabel();
-  const githubRegistrationToken = await gh.getRegistrationToken();
+  githubRegistrationToken = config.input.githubRegistrationToken;
+  if (!githubRegistrationToken) {
+    githubRegistrationToken = await gh.getRegistrationToken();
+  }  
   const ec2InstanceId = await aws.startEc2Instance(label, githubRegistrationToken);
   setOutput(label, ec2InstanceId);
   await aws.waitForInstanceRunning(ec2InstanceId);
-  await gh.waitForRunnerRegistered(label);
+  if (config.input.githubToken) {
+    await gh.waitForRunnerRegistered(label);
+  } else {
+    core.info(`Cannot check if the GitHub self-hosted runner is registered: please provide the github-token param instead of github-registration-token to check it`);
+  }
 }
 
 async function stop() {
   await aws.terminateEc2Instance();
   await gh.removeRunner();
+  if (config.input.githubToken) {
+    await gh.removeRunner();
+  } else {
+    core.info(`Cannot remove the runner: please manually remove the runner ${config.input.label} from the GitHub repository ${config.githubContext.owner}/${config.githubContext.repo}`);
+  }
 }
 
 (async function () {
